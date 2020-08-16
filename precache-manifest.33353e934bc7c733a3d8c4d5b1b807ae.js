193271b03e50d916be43fa4f112edfc453e9f4b6
Thais Gonzalez <thaisgm@umich.edu>
2020-08-16 17:35:30 -0400

    Updates

---
+++ precache-manifest.33353e934bc7c733a3d8c4d5b1b807ae.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b980a063e0bc33e9f78f9f97257809b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ddf3fab4543f2143566",
+    "url": "/static/css/2.840dafd7.chunk.css"
+  },
+  {
+    "revision": "815efada97f8365fc564",
+    "url": "/static/css/main.9ce63ca0.chunk.css"
+  },
+  {
+    "revision": "3ddf3fab4543f2143566",
+    "url": "/static/js/2.ccb6e15d.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/static/js/2.ccb6e15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815efada97f8365fc564",
+    "url": "/static/js/main.43d6b6e2.chunk.js"
+  },
+  {
+    "revision": "216c2159c4ed80420be1",
+    "url": "/static/js/runtime-main.e69abcef.js"
+  },
+  {
+    "revision": "1e97df06f0e2c2efbedac64b844be008",
+    "url": "/static/media/AwardsLogo2.1e97df06.jpg"
+  },
+  {
+    "revision": "6b4cf4eef5aacb844b35914bd9d75fbc",
+    "url": "/static/media/BETLogo.6b4cf4ee.png"
+  },
+  {
+    "revision": "5862e217e105b337b4373e3a969d667a",
+    "url": "/static/media/ComicConLogo6.5862e217.jpg"
+  },
+  {
+    "revision": "5f8128219752918173581e484325dbc8",
+    "url": "/static/media/CrunchLogo2.5f812821.png"
+  },
+  {
+    "revision": "ce39b8c48c7fa00c6f8a61bd98028407",
+    "url": "/static/media/DisneyLogo.ce39b8c4.png"
+  },
+  {
+    "revision": "79c196f49ce5f5d75df7cff4d40ad356",
+    "url": "/static/media/GoodwillLogo.79c196f4.jpg"
+  },
+  {
+    "revision": "6018328b70f950d3d57457f4175fc135",
+    "url": "/static/media/HSNLogo2.6018328b.jpg"
+  },
+  {
+    "revision": "75471c1035d055cf54c806dbac7b778d",
+    "url": "/static/media/IdemitsuLogo.75471c10.png"
+  },
+  {
+    "revision": "445cfe5980ab1164142678601bfef64d",
+    "url": "/static/media/JMLogo.445cfe59.png"
+  },
+  {
+    "revision": "0b69ab3cacc32ec8749c1c3e32287b1b",
+    "url": "/static/media/LAHCLogo.0b69ab3c.jpg"
+  },
+  {
+    "revision": "62f35f3b7dbc875dc1e54d3afb1dacaf",
+    "url": "/static/media/OMagLogo2.62f35f3b.png"
+  },
+  {
+    "revision": "00f73a4624fd4cbbadb47ffdb7d99481",
+    "url": "/static/media/PrideCondoms1.00f73a46.png"
+  },
+  {
+    "revision": "b8624c29b1482077965e158c618e84b0",
+    "url": "/static/media/PrideCondoms2.b8624c29.png"
+  },
+  {
+    "revision": "ba7d3ac952c6438468cecc14f20dda11",
+    "url": "/static/media/PrideCondoms3.ba7d3ac9.png"
+  },
+  {
+    "revision": "fd4f963fd53ee2d5c181f92d8636a5eb",
+    "url": "/static/media/PrideCondoms4.fd4f963f.png"
+  },
+  {
+    "revision": "07b7bce23c67a162c56a1abfa9f8a909",
+    "url": "/static/media/PrideLogo2.07b7bce2.jpg"
+  },
+  {
+    "revision": "335fd84a634295f30cebb9e2eae0f1f5",
+    "url": "/static/media/ProjectAngel.335fd84a.jpg"
+  },
+  {
+    "revision": "4074c4c821a45e042bce3b0966dc59c5",
+    "url": "/static/media/RedCrossLogo.4074c4c8.jpg"
+  },
+  {
+    "revision": "74193c18e24609225247009c4337a51d",
+    "url": "/static/media/ShowtimeLogo.74193c18.png"
+  },
+  {
+    "revision": "e36d06ded4f06d36c6ac0783f5ac2c32",
+    "url": "/static/media/StanLeeLogo5.e36d06de.jpg"
+  },
+  {
+    "revision": "dd624f291ef7f052b03f97ce7d49e9c0",
+    "url": "/static/media/Swagger2.dd624f29.jpg"
+  },
+  {
+    "revision": "f1fb6608e580d8127f694993500aab35",
+    "url": "/static/media/Swagger3.f1fb6608.jpg"
+  },
+  {
+    "revision": "0732a0b30f271392566c3ada775c31d9",
+    "url": "/static/media/Swagger4.0732a0b3.jpg"
+  },
+  {
+    "revision": "37b608d20f034db8b6d91977bab6d23f",
+    "url": "/static/media/SwaggerLogo2.37b608d2.jpg"
+  },
+  {
+    "revision": "f8bca34b2a14f384994d2fbc4986cac1",
+    "url": "/static/media/YMCALogo.f8bca34b.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "4dc827a9f805a25e709b7c86661f243d",
+    "url": "/static/media/monster.4dc827a9.png"
+  }
+]);
