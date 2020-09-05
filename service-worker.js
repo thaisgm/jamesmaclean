a25e192743df63dc5a7945b98691d19ac6252ad8
Thais Gonzalez <thaisgm@umich.edu>
2020-09-05 13:28:02 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04f42e65ddcb80662e6200c9b7a8edd9.js"
+  "/precache-manifest.74922e4b5f5c8160e084bb6c49cd3882.js"
 );
 
 self.addEventListener('message', (event) => {
