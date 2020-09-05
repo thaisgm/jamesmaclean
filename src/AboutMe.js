a2986a32f542ee83ddddd35e09a7e2562aa87626
Thais Gonzalez <thaisgm@umich.edu>
2020-09-05 12:54:49 -0400

    update about me page

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,38 +2,39 @@ import React from 'react';
 import TextLoop from "react-text-loop";
 import './aboutMe.css';
 
-import Monster from './images/monster.png';
+import JamesPic from './images/JamesPortraitCartoon.png';
 
 function AboutMe() {
   return (
     <div className="App-About">
-      <div className="first-container">
-        <div className="column-container">
-          <div className="textGroup">
-            <span className="noLoopText"> Hi, I'm </span>
-            <TextLoop interval="3000">
-                <div className="loopText"> James Maclean</div>
-                <div className="loopText"> Innovative</div>
-                <div className="loopText"> Resourceful</div>
-                <div className="loopText"> Motivated</div>
-            </TextLoop>
-            <span className="noLoopText">. </span>
+      <div className="container">
+        <div className="firstRow">
+        <div className="textGroup">
+          <span className="noLoopText"> I'm </span>
+          <TextLoop interval="3000">
+              <div className="loopText"> James Maclean</div>
+              <div className="loopText"> Innovative</div>
+              <div className="loopText"> Resourceful</div>
+              <div className="loopText"> Motivated</div>
+          </TextLoop>
+          <span className="noLoopText">. </span>
+        </div>
+        </div>
+        <div className="secondRowContainer">
+          <div className="secondRowA">
+            <p className="aboutMe">About Me</p>
+            <div>___________________</div>
           </div>
-          <div className="second-container">
-            <div className="aboutMeTitle">
-                <h2>About Me</h2>
-                <div>___________________</div>
-            </div>
-            <div className="aboutMeText">
-              <p>Hi, my name is James and I live in LA. Since graduating from Northeastern University
-              with BA in Business, I have had the opportunity to work with multiple brands on national projects.
-              Review my experience page for more information!
-              </p>
-            </div>
+          <div className="secondRowB">
+            <p className="aboutMeText">Born in Boston, raised on a family farm in heart of Nova Scotia and
+            creating collaboration ever since. With incredible perseverance comes amazing
+            opportunity and we've had the great fortune to work with some of our countries
+            best brands. Visit my experience page to see my favorites and ask for letters of recommendation!
+            </p>
           </div>
         </div>
-        <div className="jamesPortrait">
-          <img className="jamesPic" src={Monster} alt=""/>
+        <div className="thirdRow">
+          <img className="jamesPic" src={JamesPic} alt=""/>
         </div>
       </div>
     </div>
